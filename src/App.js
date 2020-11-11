0247d68ba96304c94fe71196ca5f0654adfd2a0e
yeongjunekoh <rhdudwns0@naver.com>
2020-11-11 21:03:30 +0900

    Creating My first React App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from "react";
-import Potato from './Potato';
 
-function App() {
+function Food({name,image}){
+  return <div>
+  <h2> I love {name} </h2>;
+  <img src = {image}/>
+
+  </div>;
+}
+
+const FoodILike = [
+{
+  name: "Kimchi",
+  image:
+    "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+},
+{
+  name: "Samgyeopsal",
+  image:
+    "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+},
+{
+  name: "Bibimbap",
+  image:
+    "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+},
+{
+  name: "Doncasu",
+  image:
+    "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+},
+{
+  name: "Kimbap",
+  image:
+    "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+}]
+
+function App(Props) {
   return (
+    <div>
     <h1 >
-      Hello!!
-      <Potato/>
     </h1>
+    {FoodILike.map(dish => 
+      <Food name = {dish.name} image = {dish.image}/>
+    )}
+    </div>
   );
 }
 
-export default App;
+export default App; 
