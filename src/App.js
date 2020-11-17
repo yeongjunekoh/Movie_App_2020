22a0d299fa51c5089ed4adbdf746723496dd036d
yeongjunekoh <rhdudwns0@naver.com>
2020-11-17 19:07:16 +0900

    Creating My First React App

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { ReactComponent } from "./logo.svg";
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count: 0
-  };
-  // 아래는 JS코드임을 알아 둘 것
-  add = () =>{
-    console.log("add");
-    this.setState(current => ({count: current.count + 1}));
-  }
-  minus = () =>{
-    console.log("minus");
-    this.setState(current => ({count: current.count - 1}));
+    isLoading: true,
+    movies: []
   }
   componentDidMount(){
-    console.log("component rendered");
-  }
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-  componentWillMount(){
-    console.log("Goodbye, cruel world");
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    },6000);
   }
   render(){
-    console.log("I'm rendering");
+    const { isLoading } = this.state; 
     return(
       <div>
-        <h1>
-        The number is {this.state.count}
-        </h1>
-        <button onClick = { this.add }>Add</button>
-        <button onClick = { this.minus }>Minus</button>
+        {isLoading ? "Loading...." : "We are Ready"}
       </div>
     );
   }
