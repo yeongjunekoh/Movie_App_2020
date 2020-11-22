79a96f1a564525cc217afe9a66304a96ad565709
yeongjunekoh <rhdudwns0@naver.com>
2020-11-22 20:52:13 +0900

    Creating My First React App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component{
   state = {
     isLoading: true,
     movies: []
   }
+  getMovie = async() =>{
+    try{
+      const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    }catch(err){
+      throw err;
+    }
+  }
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    },6000);
+    this.getMovie();
   }
   render(){
     const { isLoading } = this.state; 
