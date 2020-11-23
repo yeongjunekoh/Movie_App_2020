681621c012daceda24082992c9ee4dc1909e638d
yeongjunekoh <rhdudwns0@naver.com>
2020-11-23 13:52:43 +0900

    Creating My Firste React App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./movies";
 
 class App extends React.Component{
   state = {
@@ -8,19 +9,37 @@ class App extends React.Component{
   }
   getMovie = async() =>{
     try{
-      const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+      const {
+        data: {
+          data:{movies}
+        }
+      } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+      console.log(movies);
+      this.setState({movies, isLoading:false});
     }catch(err){
       throw err;
     }
+ 
   }
   componentDidMount(){
     this.getMovie();
   }
   render(){
-    const { isLoading } = this.state; 
+    const { isLoading, movies } = this.state; 
     return(
       <div>
-        {isLoading ? "Loading...." : "We are Ready"}
+        {isLoading 
+        ? "Loading...." 
+        : movies.map(movie => (
+          <Movie 
+          key = {movie.id} 
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image}/
+          >
+        ))}
       </div>
     );
   }
