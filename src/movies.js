bc7b337f2288960181f619f3cf84048f290a5818
yeongjunekoh <rhdudwns0@naver.com>
2020-11-24 20:11:48 +0900

    Creating My First React App

--- src/movies.js
+++ src/movies.js
@@ -2,14 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./movie.css"
 
-function Movie({year,title,summary,poster}){
+function Movie({year,title,summary,poster,genres}){
     return(
-        <div class = "movie">
+        <div className = "movie">
             <img src={poster} alt = {title} title = {title}/>
-            <div class = "movie__data">
-                <h3 class = "movie__title" style = {{backgroundColor:"red"}}>{title}</h3>
-                <h5 class = "movie__year">{year}</h5>
-                <p class = "summary">{summary}</p>
+            <div className = "movie__data">
+                <h3 className = "movie__title" style = {{backgroundColor:"red"}}>{title}</h3>
+                <h5 className = "movie__year">{year}</h5>
+                <ul className = "genres">
+                    {genres.map((genre, index) => 
+                    <li key={index} className="genres__genre">{genre}</li>
+                    )}
+                </ul>
+                <p className = "summary">{summary}</p>
             </div>
         </div>
 
@@ -22,6 +27,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster:PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
